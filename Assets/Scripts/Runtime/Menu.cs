3ebdda3f312ec3cfd89ff27fe12f53872a10e472
João Borks <joao.borks@gmail.com>
2023-05-06 00:35:57 -0300

    improve main menu

--- Assets/Scripts/Runtime/Menu.cs
+++ Assets/Scripts/Runtime/Menu.cs
@@ -1,34 +1,8 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class Menu : MonoBehaviour
 {
-    public Image splash;
-
-    Image logo;
-
-    void Start()
-    {
-        logo = splash.transform.GetChild(0).GetComponent<Image>();
-        StartCoroutine(SplashFade());
-    }
-
-    IEnumerator SplashFade()
-    {
-        yield return new WaitForSeconds(2);
-        float time = 0;
-        while (time < 2)
-        {
-            time += Time.deltaTime;
-            logo.color = Color.white - Color.white * (time / 2);
-            yield return null;
-        }
-        logo.color = Color.clear;
-        splash.gameObject.SetActive(false);
-    }
-
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
---
+++ Assets/Scripts/Runtime/UI/SplashFader.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Recall.UI
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    [DisallowMultipleComponent]
+    public class SplashFader : MonoBehaviour
+    {
+        [SerializeField, Range(.5f, 3)]
+        float _fadeTime = 2;
+        [SerializeField, Range(.1f, 3)]
+        float _showTime = 2;
+        [SerializeField]
+        AnimationCurve _fadeCurve;
+        [SerializeField]
+        CanvasGroup _logoGroup;
+
+        CanvasGroup _mainGroup;
+
+        void Awake()
+        {
+            _mainGroup = GetComponent<CanvasGroup>();
+        }
+
+        void OnEnable()
+        {
+            _mainGroup.alpha = 1;
+            FadeSplash();
+        }
+
+        Coroutine FadeSplash()
+        {
+            return StartCoroutine(splashFadeRoutine());
+            IEnumerator splashFadeRoutine()
+            {
+                _mainGroup.alpha = 1;
+                yield return FadeToAlpha(1, _logoGroup);
+                yield return new WaitForSeconds(_showTime);
+                yield return FadeToAlpha(0, _mainGroup);
+                gameObject.SetActive(false);
+            }
+        }
+
+        IEnumerator FadeToAlpha(float value, CanvasGroup group)
+        {
+            float source = group.alpha;
+            float target = value;
+
+            var endOfFrame = new WaitForEndOfFrame();
+            float time = 0;
+            while (time < _fadeTime)
+            {
+                time += Time.deltaTime;
+                group.alpha = Mathf.Lerp(source, target, _fadeCurve.Evaluate(time / _fadeTime));
+                yield return endOfFrame;
+            }
+        }
+    }
+}
